8cbf2d037bdb8a98afe321f667ab3180925dae7f
ValerySholomitskaya <Valery.Sholomitskaya@gmail.com>
2014-10-24 12:51:17 +0300

    Day1

---
+++ Day1/ArrayHelper.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionsWithArray
+{
+    public static class ArrayHelper
+    {
+        #region private static methods
+        private static void checkArray(Int32[] array)
+        {
+            if (array == null || array.Length == 0)
+                throw new ArgumentOutOfRangeException("array is null or has no elements");
+
+        }
+        private static Int32[] Merge(Int32[] arrayInFirstPart, Int32[] arrayInSecondPart)
+        {
+            Int32 a = 0, b = 0;
+            Int32[] mergedArray = new Int32[arrayInFirstPart.Length + arrayInSecondPart.Length];
+            for (Int32 i = 0; i < arrayInFirstPart.Length + arrayInSecondPart.Length; i++)
+            {
+                if (b < arrayInSecondPart.Length && a < arrayInFirstPart.Length)
+                    if (arrayInFirstPart[a] > arrayInSecondPart[b])
+                    {
+                        mergedArray[i] = arrayInSecondPart[b++];
+                    }
+                    else
+                    {
+                        mergedArray[i] = arrayInFirstPart[a++];
+                    }
+                else
+                    if (b < arrayInSecondPart.Length)
+                    {
+                        mergedArray[i] = arrayInSecondPart[b++];
+                    }
+                    else
+                    {
+                        mergedArray[i] = arrayInFirstPart[a++];
+                    }
+            }
+            return mergedArray;
+        }
+        private static Int32[] MergeSort(Int32[] array)
+        {
+            if (array.Length == 1) return array;
+            Int32 middleIndex = array.Length / 2;
+            return Merge(MergeSort(array.Take(middleIndex).ToArray()), MergeSort(array.Skip(middleIndex).ToArray()));
+        }
+        private static Int32[] QuickSortByIncrease(Int32[] array, Int32 leftIndex, Int32 rightIndex)
+        {
+            Int32 temp;
+            Int32 x = array[(leftIndex + rightIndex) / 2];
+            Int32 i = leftIndex;
+            Int32 j = rightIndex;
+            while (i <= j)
+            {
+                while (array[i] < x) i++;
+                while (array[j] > x) j--;
+                if (i <= j)
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+            if (i < rightIndex)
+            {
+                QuickSortByIncrease(array, i, rightIndex);
+            }
+
+            if (leftIndex < j)
+            {
+                QuickSortByIncrease(array, leftIndex, j);
+            }
+            return array;
+        }
+        private static Int32[] QuickSortByDecrease(Int32[] array, Int32 leftIndex, Int32 rightIndex)
+        {
+            Int32 temp;
+            Int32 x = array[(leftIndex + rightIndex) / 2];
+            Int32 i = leftIndex;
+            Int32 j = rightIndex;
+            while (i <= j)
+            {
+                while (array[i] > x) i++;
+                while (array[j] < x) j--;
+                if (i <= j)
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+            if (i < rightIndex)
+            {
+                QuickSortByIncrease(array, i, rightIndex);
+            }
+
+            if (leftIndex < j)
+            {
+                QuickSortByIncrease(array, leftIndex, j);
+            }
+            return array;
+        }
+        #endregion
+        #region public static methods
+        public static void sortByMerge(ref Int32[] array)
+        {
+            checkArray(array);
+            array = MergeSort(array);
+            /*
+                 Int32[] sortedArray = MergeSort(array);
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = sortedArray[i];
+                }
+             * */
+        }
+        public static void QuickSortByIncrease(ref Int32[] array)
+        {
+            checkArray(array);
+            array=QuickSortByIncrease(array, 0, array.Length - 1);
+/*
+            Int32[] sortedArray = QuickSortByIncrease(array, 0, array.Length - 1);
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = sortedArray[i];
+            }
+         */
+        }
+        public static void QuickSortByDecrease(Int32[] array)
+        {
+            checkArray(array);
+            array = QuickSortByDecrease(array, 0, array.Length - 1);
+            /*
+            Int32[] sortedArray = QuickSortByDecrease(array, 0, array.Length - 1);
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = sortedArray[i];
+            }
+             * */
+
+        }
+        #endregion
+
+    }
+}
---
+++ Day1/MatrixHelper.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionsWithArray
+{
+    public static class MatrixHelper
+    {
+        #region private static mathods
+        private static void checkMatrix(Int32[][] matrix)
+        {
+            if (matrix == null || matrix.Length == 0)
+            {
+                throw new ArgumentOutOfRangeException("matrix is null or has no elements");
+            }
+            if (matrix != null || matrix.Length != 0)
+            {
+                for (int i = 0; i < matrix.Length; i++)
+                {
+                    if (matrix[i] == null || matrix[i].Length == 0)
+                        throw new ArgumentOutOfRangeException("one of rows in matrix is null or has no elements");
+                }
+            }
+         }
+        private static void SwapRowsInMatrix(Int32[][] matrix, Int32 firstIndex, Int32 secondIndex)
+        {
+            Int32[] temp;
+            temp = matrix[firstIndex];
+            matrix[firstIndex] = matrix[secondIndex];
+            matrix[secondIndex] = temp;
+
+        }
+        private static Int32 SumOfRowOfMatrix(Int32[] array)
+        {
+            Int32 sum = 0;
+            for (Int32 j = 0; j < array.Length; j++)
+            {
+                sum = sum + array[j];
+            }
+            return sum;
+        }
+        private static Int32 SmallestElementInRowOfMatrix(Int32[] array)
+        {
+            Int32 smallestElement = array[0];
+            for (Int32 j = 0; j < array.Length; j++)
+            {
+                if (smallestElement > array[j])
+                {
+                    smallestElement = array[j];
+                }
+            }
+            return smallestElement;
+        }
+        private static Int32 GreatestElementInRowOfMatrix(Int32[] array)
+        {
+            Int32 greatestElement = array[0];
+            for (Int32 j = 0; j < array.Length; j++)
+            {
+                if (greatestElement < array[j])
+                {
+                    greatestElement = array[j];
+                }
+            }
+            return greatestElement;
+        }
+        private static Int32[][] SortBySumByIncrease(Int32[][] matrix)
+        {
+            for (Int32 i = 0; i < matrix.Length - 1; i++)
+            {
+                for (Int32 j = 0; j < matrix.Length - i - 1; j++)
+                {
+                    if (SumOfRowOfMatrix(matrix[j]) > SumOfRowOfMatrix(matrix[j + 1]))
+                        SwapRowsInMatrix(matrix, j, j + 1);
+                }
+            }
+            return matrix;
+        }
+        private static Int32[][] SortBySumByDecrease(Int32[][] matrix)
+        {
+            for (Int32 i = 0; i < matrix.Length - 1; i++)
+            {
+                for (Int32 j = 0; j < matrix.Length - i - 1; j++)
+                {
+                    if (SumOfRowOfMatrix(matrix[j]) < SumOfRowOfMatrix(matrix[j + 1]))
+                        SwapRowsInMatrix(matrix, j, j + 1);
+                }
+            }
+            return matrix;
+        }
+        private static Int32[][] SortBySmallestElementByIncrease(Int32[][] matrix)
+        {
+            for (Int32 i = 0; i < matrix.Length - 1; i++)
+            {
+                for (Int32 j = 0; j < matrix.Length - i - 1; j++)
+                {
+                    if (SmallestElementInRowOfMatrix(matrix[j]) > SmallestElementInRowOfMatrix(matrix[j + 1]))
+                        SwapRowsInMatrix(matrix, j, j + 1);
+                }
+            }
+            return matrix;
+        }
+        private static Int32[][] SortBySmallestElementByDecrease(Int32[][] matrix)
+        {
+            for (Int32 i = 0; i < matrix.Length - 1; i++)
+            {
+                for (Int32 j = 0; j < matrix.Length - i - 1; j++)
+                {
+                    if (SmallestElementInRowOfMatrix(matrix[j]) < SmallestElementInRowOfMatrix(matrix[j + 1]))
+                        SwapRowsInMatrix(matrix, j, j + 1);
+                }
+            }
+            return matrix;
+        }
+        private static Int32[][] SortByGreatestElementByIncrease(Int32[][] matrix)
+        {
+            for (Int32 i = 0; i < matrix.Length - 1; i++)
+            {
+                for (Int32 j = 0; j < matrix.Length - i - 1; j++)
+                {
+                    if (GreatestElementInRowOfMatrix(matrix[j]) > GreatestElementInRowOfMatrix(matrix[j + 1]))
+                        SwapRowsInMatrix(matrix, j, j + 1);
+                }
+            }
+            return matrix;
+        }
+        private static Int32[][] SortByGreatestElementByDecrease(Int32[][] matrix)
+        {
+            for (Int32 i = 0; i < matrix.Length - 1; i++)
+            {
+                for (Int32 j = 0; j < matrix.Length - i - 1; j++)
+                {
+                    if (GreatestElementInRowOfMatrix(matrix[j]) < GreatestElementInRowOfMatrix(matrix[j + 1]))
+                        SwapRowsInMatrix(matrix, j, j + 1);
+                }
+            }
+            return matrix;
+        }
+        #endregion
+        #region public static methods
+        public static void SortBySumByIncreaseInMatrix(ref Int32[][] matrix)
+        {
+            checkMatrix(matrix);
+            matrix = SortBySumByIncrease(matrix);
+            /*
+            Int32[][] sortedMatrix = SortBySumByIncrease(matrix);
+            for (Int32 i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] = sortedMatrix[i][j];
+                }
+            }
+             * */
+        }
+        public static void SortBySumByDecreaseInMatrix(ref Int32[][] matrix)
+        {
+            checkMatrix(matrix);
+            matrix = SortBySumByDecrease(matrix); ;
+            /*
+            Int32[][] sortedMatrix = SortBySumByDecrease(matrix);
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] = sortedMatrix[i][j];
+                }
+            }
+             * */
+        }
+        public static void SortBySmallestElementByIncreaseInMatrix(ref Int32[][] matrix)
+        {
+            checkMatrix(matrix);
+            matrix = SortBySmallestElementByIncrease(matrix);
+            /*
+            Int32[][] sortedMatrix = SortBySmallestElementByIncrease(matrix);
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] = sortedMatrix[i][j];
+                }
+            }
+             * */
+        }
+        public static void SortBySmallestElementByDecreaseInMatrix(ref Int32[][] matrix)
+        {
+            checkMatrix(matrix);
+            matrix = SortBySmallestElementByDecrease(matrix);
+            /*
+            Int32[][] sortedMatrix = SortBySmallestElementByDecrease(matrix);
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] = sortedMatrix[i][j];
+                }
+            }
+             * */
+        }
+        public static void SortByGreatestElementByIncreaseInMatrix(ref Int32[][] matrix)
+        {
+            checkMatrix(matrix);
+            matrix = SortByGreatestElementByIncrease(matrix);
+            /*
+            Int32[][] sortedMatrix = SortByGreatestElementByIncrease(matrix);
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] = sortedMatrix[i][j];
+                }
+            }
+             * */
+        }
+        public static void SortByGreatestElementByDecreaseInMatrix(ref Int32[][] matrix)
+        {
+            checkMatrix(matrix);
+            matrix = SortByGreatestElementByDecrease(matrix);
+            /*
+            Int32[][] sortedMatrix = SortByGreatestElementByDecrease(matrix);
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] = sortedMatrix[i][j];
+                }
+            }
+             * */
+        }
+        #endregion
+       
+    }
+}
